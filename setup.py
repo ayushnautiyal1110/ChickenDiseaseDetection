b0ebed3f09965a02403f0ce1ab3b8119c71f6445
Ayush <ayushnautiyal1110@gmail.com>
2023-06-13 13:08:36 +0530

    Requirements Added

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+with open("README.md","r",encoding="utf-8") as f:
+    long_description=f.read()
+    
+__version__="0.0.0"
+REPO_NAME="cnnClassifier"
+AUTHOR_USER_NAME="ayushnautiyal1110"
+SRC_REPO="cnnClassifier"
+AUTHOR_EMAIL="ayushnautiyal1110@gmail.com"
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="CHICKENDISEASESETECTION",
+    Long_description=long_description,
+    # Long_description_content=ong
+    Long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker":f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}"
+    },
+    package_dir={"":"src"},
+    packages=setuptools.find_packages(where="src")
+)
--- template.py
+++ template.py
@@ -20,6 +20,7 @@ list_of_files=[
       "requirement.txt",
       "setup.py",
       "research/trials.ipynb",
+      "templates/index.html"
 ]
 for filepath in list_of_files:
     filepath=Path(filepath)
@@ -27,7 +28,7 @@ for filepath in list_of_files:
     if filedir != "":
         os.makedirs(filedir,exist_ok=True)
         logging.info(f"Creating Directory: {filedir} for the file {filename}")
-    if ((not os.path.exists(filepath)) or os.path.getsize()==0):
+    if ((not os.path.exists(filepath)) or os.path.getsize(filepath)==0):
         with open(filepath,'w') as f:
             pass
             logging.info(f"Creating empty file :{filepath}")
